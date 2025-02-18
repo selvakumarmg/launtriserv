e90e1fdc3a1f3f029ffcd1dd06ea50b99014a070
selvakumarmg <selvaa0115@gmail.com>
2025-02-18 21:30:06 +0530

    added

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const app = require('./src/routes');
-
 const PORT = 3000;
 
-app.get('/api/v1/test', (req, res) => {
-  res.send('Launtri API Hosted');
-})
+app.get('/v1/test', (req, res) => {
+  res.status(200).send({
+    message: 'Launtri API Hosted',
+    status: 'success',
+    code: 200
+  });
+});
 
+// Server setup
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
