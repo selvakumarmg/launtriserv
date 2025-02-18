b5429bca3c1723785db47e131cfceb1f347bc14c
selvakumarmg <selvaa0115@gmail.com>
2025-02-18 21:54:38 +0530

    added http

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const app = require('./src/routes');
+const http = require('http'); // Import the http module
+const app = require('./src/routes'); // Assuming your routes are in 'src/routes'
 const PORT = 3000;
 
+const server = http.createServer(app); // Create the HTTP server using your app
+
+// Define the route
 app.get('/v1/test', (req, res) => {
   res.status(200).send({
     message: 'Launtri API Hosted',
@@ -9,7 +13,7 @@ app.get('/v1/test', (req, res) => {
   });
 });
 
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// Start the server to listen on http://0.0.0.0 (accessible externally) and the specified port
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
